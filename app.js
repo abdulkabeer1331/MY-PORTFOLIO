85421d10bbacbac2f0641ec2852b918ef0482870
Abdul Kabeer <abdulkabeer1331@gmail.com>
2025-03-23 03:14:18 +0500

    MY PORTFOLIO

---
+++ app.js
@@ -0,0 +1,21 @@
+const typed = new Typed(".multiple-text",{
+    strings:['Python developer','Digital Marketer','Graphic Designer','Content Creater'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 70,
+    loop:true
+})
+
+
+//phtotoshop
+//illustrator
+//premium
+//html
+//css
+//js
+//react
+//wordpress
+//node js
+//mongo dp
+//express js
+//Digital Marketing
